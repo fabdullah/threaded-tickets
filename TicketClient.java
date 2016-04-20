cf340f5c118cfe4fff63f8e7ad4c7afe3aa69f09
fabdullah <fabdullah@utexas.edu>
2016-04-19 22:49:41 -0500

    Update TicketClient.java

--- TicketClient.java
+++ TicketClient.java
@@ -36,16 +36,6 @@ public class TicketClient {
 	String result = "dummy";
 	String hostName = "";
 	String threadName = "";
-	int seatIt = 0;
-	int rowIt = 0;
-	static List<String> seats = Arrays.asList("108", "109", "110", "111", "112", "113", "114, "115", 
-						  "116", "117", "118", "119", "120", "121", "101", "102", 
-						  "103", "104", "105", "106", "107", "122", "123", "124", 
-						  "125", "126", "127", "128");
-	static List<String> frontRows = Arrays.asList("A", "B", "C", "D", "E", "F", "G, "H", "J", "K", "L", "M");
-	static List<String> backRows = Arrays.asList("N", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z");
-	static int NUMSEATS = 28;
-	static int NUMROWS = 24;
 
 	TicketClient(String hostname, String threadname) {
 		tc = new ThreadedTicketClient(this, hostname, threadname);
@@ -65,43 +55,6 @@ public class TicketClient {
 		// TODO thread.run()
 		tc.run();
 		System.out.println(hostName + "," + threadName + " got one ticket");
-	}
-	
-	synchronized void run() {
-		// If seats are avilable
-		if(seatIt < NUMSEATS && rowIt < NUMROWS) {
-			String seat = bestAvailableSeat();
-			markAvailableSeatTaken(seat);
-			printTicketSeat(seat);	
-		}
-		//Exit otherwise
-		else {
-		}
-	}
-	
-	String bestAvailableSeat() {
-		String s;
-		if(rowIt < NUMROWS/2) {
-			s = frontRows[rowIt] + seat[seatIt];
-		}
-		else {
-			s = backRows[rowIt-(NUMROWS/2)] + seat[seatIt];			
-		}
-		return s;
-	}
-	
-	void markAvailableSeatTaken(String seat) {
-		// Updates and resets counters appropriately
-		if(seatIt < NUMSEATS) {
-			 seatIt++;
-		else {
-			seatIt = 0;
-			rowIt++;
-		}
- 	}
-	
-	void printTicketSeat(String seat) {
-		
 	}
 
 	void sleep() {
